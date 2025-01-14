7957f2a02e33ce8e876dbdccad06b3bd4732541f
GameBuilder101 <gamebuilder101@gmail.com>
2025-01-13 23:50:34 -0500

    Removed scrapped logo/title animation

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,3 @@
-/*// Get the title and clear it to begin a typing animation
-const title = document.querySelector("#title a");
-const titleText = title.innerText;
-title.innerHTML = "";
-
-// Create the cursor for typing the title
-const typingCursor = document.createElement("span");
-typingCursor.className = "typing-cursor";
-typingCursor.innerText = "_";
-title.appendChild(typingCursor);
-
-let typeIndex = 0; // Establish a character index to increment
-typeTitleStep();
-
-// Types the next letter in the title animation
-function typeTitleStep() {
-    // Create the character element
-    const typedLetter = document.createElement("span");
-    typedLetter.className = "typed-letter";
-    typedLetter.innerText = titleText.charAt(typeIndex);
-
-    title.insertBefore(typedLetter, typingCursor);
-    typeIndex++;
-
-    if (typeIndex >= titleText.length) {
-        typingCursor.remove();
-    } else {
-        setTimeout(typeTitleStep, 70);
-    }
-}*/
-
 // Set up portfolio tilt card effect
 VanillaTilt.init(document.querySelectorAll(".portfolio-item"), {
     max: 5,
